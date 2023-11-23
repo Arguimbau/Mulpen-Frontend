16f9516120646fc79693b67df46781ffa29f3e4f
DizzyMoon <mikkel.simons1@gmail.com>
2023-11-23 14:27:20 +0100

    Added background color to uploadTest.html

--- js/postTest.js
+++ js/postTest.js
@@ -3,7 +3,10 @@ function submitForm(){
 
     fetch('http://localhost:8080/create-post', {
         method: 'POST',
-        body: formData
+        body: formData,
+        headers: {
+          'Content-Type': 'application/json'
+        }
     })
         .then(response => response.json())
         .then(data => {
