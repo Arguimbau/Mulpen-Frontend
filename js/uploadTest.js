5ba2a81003a57f397c83c5f4d6d38640aebd33fe
DizzyMoon <mikkel.simons1@gmail.com>
2023-11-24 18:33:04 +0100

    Made it more fancy :)

--- js/uploadTest.js
+++ js/uploadTest.js
@@ -1,10 +1,13 @@
 // upload file to site
-function uploadFile(){
+function uploadFile() {
     var fileInput = document.getElementById('fileInput');
     var file = fileInput.files[0];
     var description = document.getElementById('descriptionInput').value;
 
     var formData = new FormData();
+
+    var snackbar = document.getElementById('snackbar')
+
     formData.append('file', file);
     formData.append('description', description);
 
@@ -15,10 +18,30 @@ function uploadFile(){
     })
         .then(response => response.text())
         .then(message => {
-            document.getElementById('uploadStatus').innerHTML = message;
+
+            snackbar.innerHTML = message;
+            snackbar.className = "show";
+
+
+            setTimeout(function () {
+                snackbar.className = snackbar.className.replace("show", "");
+            }, 8000)
+
+
+
+
         })
         .catch(error => {
             console.error('Error during file upload:', error);
-            document.getElementById('uploadStatus').innerHTML = 'Error during file upload.';
+            snackbar.innerHTML = 'Error during file upload.';
+            snackbar.className = "show";
+
+            setTimeout(function () {
+                snackbar.className = snackbar.className.replace("show", "");
+            }, 8000)
+
+
         })
 }
+
+
