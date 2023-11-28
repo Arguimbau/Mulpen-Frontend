c8b71816cb41f438b48cf7daf0983dbd216abeab
DizzyMoon <mikkel.simons1@gmail.com>
2023-11-28 11:18:14 +0100

    Refactored. Now uploadView is specifically for videos. (Is now called viewVideo.html)

--- js/render_uploads.js
+++ js/render_uploads.js
@@ -3,78 +3,146 @@ const videoExtenstions = ["mp4", "mkv", "webm", "mov", "avi"];
 const audioExtenstions = ["mp3", "wav", "ogg"];
 
 function createMediaElement(url) {
-  const extension = url.split(".").pop();
+    const extension = url.split(".").pop();
 
-  const div = document.createElement("div");
-/*
-  const imageText = document.createElement("div");
-  imageText.classList.add("desc")
-  imageText.textContent = description;
+    const div = document.createElement("div");
+    /*
+      const imageText = document.createElement("div");
+      imageText.classList.add("desc")
+      imageText.textContent = description;
 
- */
+     */
 
-  if (imageExtenstions.includes(extension)) {
-    const img = document.createElement("img");
-    img.src = url;
-    img.alt = "Image";
-    div.appendChild(img);
-    console.log(div)
-  } else if (videoExtenstions.includes(extension)) {
-    const video = document.createElement("video");
-    video.width = 320;
-    video.height = 240;
-    video.controls = true;
-    const source = document.createElement("source");
-    source.src = url;
-    source.type = "video/mp4";
-    video.appendChild(source);
-    div.appendChild(video);
-  } else if (audioExtenstions.includes(extension)) {
-    const audio = document.createElement("audio");
-    audio.controls = true;
-    const source = document.createElement("source");
-    source.src = url;
-    source.type = "audio/mpeg";
-    audio.appendChild(source);
-    div.appendChild(audio);
-  }
+    if (imageExtenstions.includes(extension)) {
+        const img = document.createElement("img");
+        img.src = url;
+        img.alt = "Image";
+        div.appendChild(img);
+        console.log(div)
+    } else if (videoExtenstions.includes(extension)) {
+        const video = document.createElement("video");
+        video.controls = false;
+        video.width = 320;
+        video.height = 240;
+        const source = document.createElement("source");
+        source.src = url;
+        source.type = "video/mp4";
+        video.appendChild(source);
+        div.appendChild(video);
 
-  return div;
+
+
+
+
+
+
+
+    } else if (audioExtenstions.includes(extension)) {
+        const audio = document.createElement("audio");
+        audio.controls = true;
+        const source = document.createElement("source");
+        source.src = url;
+        source.type = "audio/mpeg";
+        audio.appendChild(source);
+        div.appendChild(audio);
+    }
+
+    return div;
 }
 
 async function loadMedias() {
-  const medias = await fetch(`${API_BASE}/media/all`, {
-    method: "GET",
-    mode: "cors"
-  });
+    const medias = await fetch(`${API_BASE}/media/all`, {
+        method: "GET",
+        mode: "cors"
+    });
 
-  const mediaContainer = document.getElementById("media-container");
-  if (!medias.ok) {
-    mediaContainer.textContent = "Error";
-    return;
-  }
+    const mediaContainer = document.getElementById("media-container");
+    if (!medias.ok) {
+        mediaContainer.textContent = "Error";
+        return;
+    }
 
-  for ( const media of await medias.json() ) {
-    const imageText = document.createElement("div");
-    imageText.classList.add("desc")
-    imageText.textContent = media.description
+    for (const media of await medias.json()) {
+        const imageText = document.createElement("div");
+        imageText.classList.add("desc")
+        imageText.textContent = media.description
 
-    const fullImage = document.createElement("a");
-    fullImage.href = `${API_BASE}/media/upload/${encodeURIComponent(media.filePath)}`
+        const fullImage = document.createElement("a");
+        fullImage.classList.add("image-popout")
+        fullImage.href = `${API_BASE}/media/upload/${encodeURIComponent(media.filePath)}`
 
-    const gallery = document.createElement("div");
-    gallery.classList.add("gallery")
+        const gallery = document.createElement("div");
+        gallery.classList.add("gallery")
 
-    const newElement = createMediaElement(`${API_BASE}/media/upload/${encodeURIComponent(media.filePath)}`);
+        const newElement = createMediaElement(`${API_BASE}/media/upload/${encodeURIComponent(media.filePath)}`);
 
-    mediaContainer.appendChild(gallery)
-    gallery.appendChild(fullImage);
-    fullImage.appendChild(newElement)
-    gallery.appendChild(imageText)
-    //mediaContainer.appendChild(newElement);
-  }
+        mediaContainer.appendChild(gallery)
+        gallery.appendChild(fullImage);
+        fullImage.appendChild(newElement)
+        gallery.appendChild(imageText)
+        //mediaContainer.appendChild(newElement);
+    }
 }
 
-document.addEventListener("DOMContentLoaded", async function() {
-  await loadMedias();
+document.addEventListener("DOMContentLoaded", async function () {
+    await loadMedias();
+
+
+    // Get all video elements on the page
+    const videos = document.querySelectorAll('video');
+
+// Function to start the preview for a specific video
+    function startPreview(video) {
+        video.muted = true;
+        video.currentTime = 1;
+        video.playbackRate = 0.5;
+        video.play();
+    }
+
+// Function to stop the preview for a specific video
+    function stopPreview(video) {
+        video.currentTime = 0;
+        video.playbackRate = 0.1;
+        video.pause();
+    }
+
+// Event listeners for each video element
+    videos.forEach((video) => {
+        let previewTimeout = null;
+
+        video.addEventListener('mouseenter', () => {
+            startPreview(video);
+            previewTimeout = setTimeout(() => stopPreview(video), 4000);
+        });
+
+        video.addEventListener('mouseleave', () => {
+            clearTimeout(previewTimeout);
+            previewTimeout = null;
+            stopPreview(video);
+        });
+    });
+
+    /*
+    const videoContainers = document.getElementsByClassName("popup-video-container")
+
+
+    videoContainers.addEventListener("click", () => {
+        const video = this.video;
+        const videoSrc = video.querySelector('source').getAttribute('src');
+        openVideoPopup(videoSrc);
+    })
+
+    function openVideoPopup(videoSrc) {
+        modelVideo.src = videoSrc;
+        overlay.style.display = 'flex';
+    }
+
+    function closeVideoPopup() {
+        modelVideo.src = '';
+        overlay.style.display = 'none';
+    }
+
+    closeBtn.addEventListener('click', closeVideoPopup())
+
+     */
 });
