28320a8eae1cf419e0fa07517223ef671a7fb111
Geargrind <lass347s@stud.kea.dk>
2023-11-29 12:14:17 +0100

    Tilføjet billeder, samt rettet CSS og html.

---
+++ js/dashboard.js
@@ -0,0 +1,6 @@
+document.getElementById("upload").addEventListener("click", function () {
+    window.location.href = "uploadMedia.html"
+});
+document.getElementById("delete").addEventListener("click", function () {
+    window.location.href = "placeholder.html"
+});
