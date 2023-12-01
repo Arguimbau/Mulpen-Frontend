7bce47c9e61c928be73139c4b4e9d09688e8bb2b
lillebitteditte <ditt2047@stud.kea.dk>
2023-12-01 12:58:52 +0100

    Slideshow on the way

---
+++ js/slideshow.js
@@ -0,0 +1,20 @@
+let slideIndex = 0;
+showSlides();
+let slideshow = document.getElementById("mySlides");
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+
+}
+
+
+setTimeout(function () {
+  slideshow.className.replace("show", "");
+}, 8000)
