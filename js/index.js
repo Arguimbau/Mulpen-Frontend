1b46e4dc14373e5a8842413444443b31e264ec6c
weix0015 <weix0015@stud.kea.dk>
2023-12-04 11:27:13 +0100

    added translate danish to english function on index.html

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
+// language switch
+
+function switchLanguage(language) {
+    var productionLinkElement = document.getElementById("productionLink");
+    var contactLinkElement = document.getElementById("contactLink");
+    var aboutLinkElement = document.getElementById("aboutLink");
+    var descriptionElement = document.getElementById("description");
+
+    if (language === 'da') {
+        productionLinkElement.innerHTML = '<h3>produktioner</h3>';
+        contactLinkElement.innerHTML = '<h3>kontakt</h3>';
+        aboutLinkElement.innerHTML = '<h3>om mulpen</h3>';
+
+        descriptionElement.innerHTML = 'Vi er et kreativt selskab<br>der udvikler og producerer<br>' +
+          'underholdning.<br><br>Er vi ikke søde?<br><br><label for="Ja">Tjo</label><input type="checkbox" ' +
+          'id="Ja"><label for="VedIkke">Ved ikke</label><input type="checkbox" id="VedIkke">' +
+          '<label for="Naeh">Næh</label><input type="checkbox" id="Naeh">';
+
+    } else if (language === 'en') {
+        productionLinkElement.innerHTML = '<h3>productions</h3>';
+        contactLinkElement.innerHTML = '<h3>contact</h3>';
+        aboutLinkElement.innerHTML = '<h3>about mulpen</h3>';
+
+        descriptionElement.innerHTML = 'We are a creative company<br>developing and producing<br>' +
+          'entertainment.<br><br>Aren\'t we sweet?<br><br><label for="Ja">Sure</label><input type="checkbox" ' +
+          'id="Ja"><label for="VedIkke">Not sure</label><input type="checkbox" id="VedIkke">' +
+          '<label for="Naeh">Nah</label><input type="checkbox" id="Naeh">';
+    }
+}
 
     $(document).ready(function() {
     function redirectToPage(checkboxId, destinationUrl) {
