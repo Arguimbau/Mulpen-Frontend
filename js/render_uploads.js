77f5d73813ad2451fa924d24c294c05a0ba3feba
Arthur <arth3608@stud.kea.dk>
2023-12-06 18:37:28 +0100

    Fixed delete function

--- js/render_uploads.js
+++ js/render_uploads.js
@@ -8,9 +8,9 @@ function createDeleteButton(id) {
     deleteButton.textContent = "Delete";
 
     // Prevent the default anchor behavior
-    deleteButton.addEventListener("click", (event) => {
+    deleteButton.addEventListener("click", async (event) => {
         event.preventDefault();
-        deleteMedia(id);
+        await deleteMedia(id);
     });
 
     div.appendChild(deleteButton);
@@ -33,16 +33,15 @@ async function deleteMedia(id) {
         const mediaElement = document.getElementById(`media-${id}`);
         if (mediaElement) {
             mediaElement.remove();
+            location.reload();
+
         }
-        mediaElement.description.remove();
-        // Refresh the page
-        //location.reload();
     } catch (error) {
         console.error("Error deleting media", error);
     }
 }
 
-function createMediaElement(url, id) {
+function createMediaElement(url, id, description) {
     const extension = url.split(".").pop();
 
     const div = document.createElement("div");
@@ -51,6 +50,8 @@ function createMediaElement(url, id) {
         const img = document.createElement("img");
         img.src = url;
         img.alt = "Image";
+        img.style.width = "250px";
+        img.style.height = "250px";
         div.appendChild(img);
     } else if (videoExtensions.includes(extension)) {
         const video = document.createElement("video");
@@ -72,6 +73,16 @@ function createMediaElement(url, id) {
         div.appendChild(audio);
     }
 
+    // Display media description
+    const imageText = document.createElement("div");
+    imageText.classList.add("desc");
+    imageText.textContent = description || ""; // Ensure description is not undefined
+    div.appendChild(imageText);
+
+    // Attach the event listener directly to the delete button
+    const deleteButton = createDeleteButton(id);
+    div.appendChild(deleteButton);
+
     return div;
 }
 
@@ -92,26 +103,10 @@ async function loadMedias() {
         for (const media of medias) {
             const gallery = document.createElement("div");
             gallery.classList.add("gallery");
-            const mediaElement = createMediaElement(`${API_BASE}/media/upload/${encodeURIComponent(media.filePath)}`, media.id);
+            const mediaElement = createMediaElement(`${API_BASE}/media/upload/${encodeURIComponent(media.filePath)}`, media.id, media.description);
             mediaElement.id = `media-${media.id}`;
-            const fullImage = document.createElement("a");
-            fullImage.classList.add("image-popout");
-            fullImage.href = `${API_BASE}/media/upload/${encodeURIComponent(media.filePath)}`;
-
-            const imageText = document.createElement("div");
-            imageText.classList.add("desc");
-            imageText.textContent = media.description;
-
-            fullImage.appendChild(mediaElement);
-            gallery.appendChild(fullImage);
-            gallery.appendChild(imageText);
-
-            // Attach the event listener directly to the delete button
-            const deleteButton = createDeleteButton(media.id);
-            deleteButton.addEventListener("click", () => deleteMedia(media.id));
-            gallery.appendChild(deleteButton);
-
             mediaContainer.appendChild(gallery);
+            gallery.appendChild(mediaElement);
         }
     } catch (error) {
         console.error(error);
@@ -154,114 +149,4 @@ document.addEventListener("DOMContentLoaded", async function () {
             stopPreview(video);
         });
     });
-});
-
-const imageExtenstions = ["jpg", "jpeg", "png", "gif", "webp"];
-const videoExtenstions = ["mp4", "mkv", "webm", "mov", "avi"];
-const audioExtenstions = ["mp3", "wav", "ogg"];
-
-function createMediaElement(url) {
-  const extension = url.split(".").pop();
-
-  const div = document.createElement("div");
-
-  if (imageExtenstions.includes(extension)) {
-    const img = document.createElement("img");
-    img.src = url;
-    img.alt = "Image";
-    div.appendChild(img);
-    console.log(div)
-  } else if (videoExtenstions.includes(extension)) {
-    const video = document.createElement("video");
-    video.controls = false;
-    video.width = 320;
-    video.height = 240;
-    const source = document.createElement("source");
-    source.src = url;
-    source.type = "video/mp4";
-    video.appendChild(source);
-    div.appendChild(video);
-  } else if (audioExtenstions.includes(extension)) {
-    const audio = document.createElement("audio");
-    audio.controls = true;
-    const source = document.createElement("source");
-    source.src = url;
-    source.type = "audio/mpeg";
-    audio.appendChild(source);
-    div.appendChild(audio);
-  }
-
-  return div;
-}
-
-async function loadMedias() {
-  const medias = await fetch(`${API_BASE}/media/all`, {
-    method: "GET",
-    mode: "cors"
-  });
-
-  const mediaContainer = document.getElementById("media-container");
-  if (!medias.ok) {
-    mediaContainer.textContent = "Error";
-    return;
-  }
-
-  for (const media of await medias.json()) {
-    const imageText = document.createElement("div");
-    imageText.classList.add("desc")
-    imageText.textContent = media.description
-
-    const fullImage = document.createElement("a");
-    fullImage.classList.add("image-popout")
-    fullImage.href = `${API_BASE}/media/upload/${encodeURIComponent(media.filePath)}`
-
-    const gallery = document.createElement("div");
-    gallery.classList.add("gallery")
-
-    const newElement = createMediaElement(`${API_BASE}/media/upload/${encodeURIComponent(media.filePath)}`);
-
-    mediaContainer.appendChild(gallery)
-    gallery.appendChild(fullImage);
-    fullImage.appendChild(newElement)
-    gallery.appendChild(imageText)
-  }
-}
-
-document.addEventListener("DOMContentLoaded", async function () {
-  await loadMedias();
-
-
-  // Get all video elements on the page
-  const videos = document.querySelectorAll('video');
-
-// Function to start the preview for a specific video
-  function startPreview(video) {
-    video.muted = true;
-    video.currentTime = 1;
-    video.playbackRate = 0.5;
-    video.play();
-  }
-
-// Function to stop the preview for a specific video
-  function stopPreview(video) {
-    video.currentTime = 0;
-    video.playbackRate = 0.1;
-    video.pause();
-  }
-
-// Event listeners for each video element
-  videos.forEach((video) => {
-    let previewTimeout = null;
-
-    video.addEventListener('mouseenter', () => {
-      startPreview(video);
-      previewTimeout = setTimeout(() => stopPreview(video), 4000);
-    });
-
-    video.addEventListener('mouseleave', () => {
-      clearTimeout(previewTimeout);
-      previewTimeout = null;
-      stopPreview(video);
-    });
-  });
 });
