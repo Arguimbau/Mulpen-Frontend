e2ace6d960b809916b342b7773a83423018dd037
weix0015 <weix0015@stud.kea.dk>
2023-12-06 11:48:17 +0100

    added javascript to renderSlideshow.js

--- js/renderSlideshow.js
+++ js/renderSlideshow.js
@@ -1,25 +1,30 @@
-document.addEventListener("DOMContentLoaded", async function () {
-    // Get the slideshow container
-    const slideshowContainer = document.querySelector('.intro-slideshow');
+async function loadSlideshow() {
+  const slideshowContainer = document.querySelector('.intro-slideshow');
 
-    // Fetch image URLs from the server
-    const slideshow = await fetch(`${API_BASE}/slideshow/all`, {
-        method: "GET",
-        mode: "cors"
+  try {
+    const imageUrls = await fetch(`${API_BASE}/slideshow/all`, {
+      method: "GET",
+      mode: "cors"
     })
       .then(response => response.json())
-      .then(imageUrls => {
-          // Create an image element for each URL
-          imageUrls.forEach(url => {
-              const img = document.createElement('img');
-              img.src = url;
-              img.alt = 'Slideshow Image';
-              slideshowContainer.appendChild(img);
-          });
-
-          nextImage();
-      })
       .catch(error => {
-          console.error('Error fetching image URLs:', error);
+        console.error('Error fetching image URLs:', error);
+        return [];
       });
-});
+
+    // Create an image element for each URL
+    imageUrls.forEach(url => {
+      const img = document.createElement('img');
+      img.src = url;
+      img.alt = 'Slideshow Image';
+      slideshowContainer.appendChild(img);
+    });
+
+    // Start the slideshow
+    nextImage()
+  } catch (error) {
+    console.error('Error loading slideshow:', error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", loadSlideshow);
