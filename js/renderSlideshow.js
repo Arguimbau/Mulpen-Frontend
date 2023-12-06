fafc18449ee9d43229bbe6b3830406f201db3e28
weix0015 <weix0015@stud.kea.dk>
2023-12-06 12:27:59 +0100

    added javascript to renderSlideshow.js

--- js/renderSlideshow.js
+++ js/renderSlideshow.js
@@ -2,18 +2,24 @@ async function loadSlideshow() {
   const slideshowContainer = document.querySelector('.intro-slideshow');
 
   try {
-    const imageUrls = await fetch(`${API_BASE}/slideshow/all`, {
+    const response = await fetch(`${API_BASE}/slideshow/all`, {
       method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
       mode: "cors"
-    })
-      .then(response => response.json())
-      .catch(error => {
-        console.error('Error fetching image URLs:', error);
-        return [];
-      });
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error fetching image URLs: ${response.statusText}`);
+    }
+
+    const imageUrls = await response.json();
+    console.log(imageUrls);
 
     // Create an image element for each URL
     imageUrls.forEach(url => {
+      console.log(url);
       const img = document.createElement('img');
       img.src = url;
       img.alt = 'Slideshow Image';
@@ -21,9 +27,25 @@ async function loadSlideshow() {
     });
 
     // Start the slideshow
-    nextImage()
+    startSlideshow();
   } catch (error) {
     console.error('Error loading slideshow:', error);
+  }
+}
+
+function startSlideshow() {
+  const slideshowImages = document.querySelectorAll(".intro-slideshow img");
+  const nextImageDelay = 4000;
+  let currentImageCounter = 0;
+
+  slideshowImages[currentImageCounter].style.opacity = 1;
+
+  setInterval(nextImage, nextImageDelay);
+
+  function nextImage() {
+    slideshowImages[currentImageCounter].style.opacity = 0;
+    currentImageCounter = (currentImageCounter + 1) % slideshowImages.length;
+    slideshowImages[currentImageCounter].style.opacity = 1;
   }
 }
 
