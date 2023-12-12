8501c7436f71ea314d67104566d84f2d4f66f5a7
DizzyMoon <mikkel.simons1@gmail.com>
2023-12-12 19:02:43 +0100

    Made a few changes to headers in loadMedias()

--- js/render_uploads.js
+++ js/render_uploads.js
@@ -56,6 +56,9 @@ async function loadMedias() {
     const medias = await fetch(`${API_BASE}/media/all`, {
         method: "GET",
         mode: "cors",
+        headers: {
+            'Content-Range': '0-99999999'
+        }
     });
 
     const mediaContainer = document.getElementById("media-container");
